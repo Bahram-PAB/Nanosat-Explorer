5f0fc022a2ba609938d4459596e4994428b0cc8e
Bahram Pouralibaba <bahrambaba@gmail.com>
2026-06-15 15:43:07 +0330

    Update Satellite.kt

--- app/src/main/java/com/example/data/Satellite.kt
+++ app/src/main/java/com/example/data/Satellite.kt
@@ -1,8 +1,15 @@
 package com.example.data
 
-// این کلاس شکل هر ماهواره را مشخص می‌کند
+// این کلاس قالب ماهواره‌های شما را برای برنامه مشخص می‌کند
 data class Satellite(
     val name: String,
-    val tleLine1: String,
-    val tleLine2: String
+    val organisation: String,
+    val nation: String,
+    val type: String,
+    val launchDate: String,
+    val status: String,
+    val description: String,
+    // این دو خط زیر به صورت زاپاس قرار گرفته‌اند تا اگر ظاهر برنامه به TLE نیاز داشت، خطا ندهد
+    val tleLine1: String = "",
+    val tleLine2: String = ""
 )
